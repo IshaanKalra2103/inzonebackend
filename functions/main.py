f5f6ee5d533ed772f057feede7fea08469d3200c
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2024-06-12 00:37:04 +0530

    woah
    woah

--- functions/main.py
+++
@@ -1,6 +0,0 @@
-import os
-from flask import Flask, request
-from main import app  # Import your Flask app from the root directory
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
